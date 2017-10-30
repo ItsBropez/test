8b9fe2648fceb79e5685aac5d3cf5ede4da68687
ItsBropez <itsbropez@gmail.com>
2017-10-30 09:12:02 -0600

    s

--- index.js
+++ index.js
@@ -8,19 +8,28 @@ bittrex.options({
     'apisecret' : '320ce990f19d40ea932bd9f91f347a49'
 });
 console.log('Bittrex Keys insterted');
-
+/*
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/crypto", function (err, db) {
-    if (err) {
-        throw err;
-    }
+MongoClient.connect("mongodb://localhost:27017/testing", function (err, db) {
+    assert.equal(null, err);
     console.log('Connected');
     db.close;
     //Write databse Insert/Update/Query code here..
               
-});
+}); */
 
 bittrex.getmarketsummaries(function (data, err) {
-    console.log(data.result[0].MarketName);
+    console.log(data.result);
+    
+    // Connect to the db
+    MongoClient.connect("mongodb://localhost:27017/testing", function (err, db) {
+        assert.equal(null, err);
+        console.log('Connected');
+        db.collection('results').insertOne(data.result);
+        console.log('Market Summaries Logged');
+        db.close
+        //Write databse Insert/Update/Query code here..
+
+    });
     
 });
