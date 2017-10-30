e98ee0c89303c4dc55e6cd2bce4c7182af393ca4
ItsBropez <itsbropez@gmail.com>
2017-10-30 12:05:22 -0600

    s

---
+++ Models/markets.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var marketsSchema = mongoose.Schema({
+    MarketName: String,
+    timestamp: { type: Date, default: Date.now},
+    High: Number,
+    Low: Number,
+    Last: Number,
+    BaseVolume: Number,
+    Bid: Number,
+    Ask: Number,
+    OpenBuyOrders: Number,
+    OpenSellOrders: Number,
+    },
+});
+
+var Markets = module.exports = mongoose.model('markets');
+
+//get markets
+module.exports.getMarkets() = function(callback, limit){
+    Markets.find(callback).limit(limit);
+}
