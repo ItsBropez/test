fad6a5cc78964e914a88fef6d0a355e604b2e662
ItsBropez <itsbropez@gmail.com>
2017-10-29 20:51:00 -0600

    2

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var http = require('http');
 var bit = require('node-bittrex-api')
-var mongo = require('mongodb')
+var MongoClient = require('mongodb').MongoClient;
 
-console.log('Hello');
+bit.options({
+  'apikey' : '0aafd35549524f89b442e21a1f01dc68',
+  'apisecret' : '320ce990f19d40ea932bd9f91f347a49',
+});
+console.log('Bittrex Keys insterted');
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/crypto", function (err, db) {
+   
+     if(err) throw err;
+	 console.log('Connected');
+     //Write databse Insert/Update/Query code here..
+                
+});
