e553add234f85dfcb1180851da2eba016fec0fcd
ItsBropez <itsbropez@gmail.com>
2017-10-30 12:03:36 -0600

    as

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+Markets = require('./Models/markets.js');
 //connect to mongoose
 mongoose.connect('mongodb://localhost/restful');
 var db = mongoose.connection
 
 app.get('/', function(req,res){
-    res.send('Hello World!');
+    res.send('Use /markets');
 
 });
+
+app.get('/markets', function(req,res){
+    Markets.getMarkets(function(err, markets){
+        if (err){
+            throw err
+        }
+        res.json(markets)
+    });
+});
+
 
 app.listen(3000);
 console.log('Running on port 3000...');
