948570a489fb495070f05ad2806c3f7607385706
ItsBropez <itsbropez@gmail.com>
2017-10-30 21:01:41 -0600

    a

--- client/controllers/markets.js
+++ client/controllers/markets.js
@@ -2,7 +2,7 @@ var myApp = angular.module('myApp');
 
 myApp.controller('MarketsController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
     console.log('MarketsController loaded');
-    $scope.getMarkets = funtion(){
+    $scope.getMarkets = function(){
         $http.get('/markets').success(function(responce){
             $scope.markets = responce;     
         });                     
