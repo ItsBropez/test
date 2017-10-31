3317d28bd540325e158e085ca38e0cf503f9bec9
ItsBropez <itsbropez@gmail.com>
2017-10-30 21:03:03 -0600

    a

--- client/app.js
+++ client/app.js
@@ -2,23 +2,23 @@ var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function($routeProvider){
     $routeProvider.when('/', {
-        controller:'MarketsController'
+        controller:'MarketsController',
         templateUrl: 'views/markets.html'
     })
     .when('/markets', {
-        controller:'MarketsController'
+        controller:'MarketsController',
         templateUrl: 'views/markets.html'
     })
     .when('/markets/entries/:id'), {
-        controller:'MarketsController'
+        controller:'MarketsController',
         templateUrl: 'views/market_entries.html'
     }
     .when('/markets/add'), {
-        controller:'MarketsController'
+        controller:'MarketsController',
         templateUrl: 'views/add_market.html'
     }
     .when('/markets/edit/:id'), {
-        controller:'MarketsController'
+        controller:'MarketsController',
         templateUrl: 'views/edit_market.html'
     }
     .otherwise({
