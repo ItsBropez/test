b2cc6175c15a1e72d379bdd0157764c134f6fcbd
ItsBropez <itsbropez@gmail.com>
2017-10-30 20:35:00 -0600

    a

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-app.use(express.static(_dirname + "/client"));
+app.use(express.static("./client"));
 app.use(bodyParser.json());
 
 var Markets = require('./Models/markets.js');
