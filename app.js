3d4c88b966d7930bf0fb5a0c06a300afeb7030f7
ItsBropez <itsbropez@gmail.com>
2017-11-18 09:29:30 -0700

    a

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ var info = require('./info.js');
 var https = require('https');
 var MongoClient = require('mongodb');
 
+/*
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var assert = require('assert');
+var mongoose = require('mongoose');
+*/
+
 var dbURL = info.dbURL;
 var mcURL = info.mcURL;
 var apisecret = info.apisecret;
 var apikey = info.apikey;
+
+/*
+app.use(express.static("./client"));
+app.use(bodyParser.json());
+
+var Market = require('./models/market.js');
+*/
 
 bittrex.options({
     'apikey': apikey,
@@ -80,3 +95,65 @@ runner().then(function (resp) {
 });
 
 console.log('Running on port 3000...');
+/*
+setInterval(function () {
+    updateData();
+}, 60 * 0.5 * 1000);
+    
+
+app.get('/', function (req, res) {
+    res.send('Use /markets');
+
+});
+
+app.get('/markets', function (req, res) {
+    Market.getMarkets(function (err, markets) {
+        if (err) {
+            throw err;
+        }
+        res.json(markets);
+    });
+});
+
+app.get('/markets/:_id', function (req, res) {
+    Market.getMarketById(req.params._id, function (err, market) {
+        if (err) {
+            throw err;
+        }
+        res.json(market);
+    });
+});
+
+app.post('/markets', function (req, res) {
+    var market = req.body;
+    Market.addMarket(market, function (err, market) {
+        if (err) {
+            throw err;
+        }
+        res.json(market);
+    });
+});
+
+app.put('/markets/:_id', function (req, res) {
+    var id = req.params._id;
+    var market = req.body;
+    Market.updateMarket(id, market, {}, function (err, market) {
+        if (err) {
+            throw err;
+        }
+        res.json(market);
+    });
+});
+
+app.delete('/markets/:_id', function (req, res) {
+    var id = req.params._id;
+    Market.removeMarket(id, function (err, market) {
+        if (err) {
+            throw err;
+        }
+        res.json(market);
+    });
+});
+
+app.listen(3000);
+*/
