9fdbf37a7727fa8bb939af1ba37bdd5e37bd9df9
ItsBropez <itsbropez@gmail.com>
2017-11-18 06:02:19 -0700

    a

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ bittrex.options({
     'apisecret': apisecret
 });
 
+/*
 function updateData() {
     bittrex.getmarketsummaries(function (data, err) {
         if (err) {
@@ -43,14 +44,43 @@ function updateData() {
             db1.close();
         });
     });
+} */
+
+//need to update other db with symbol and index then foreach item in data.result add index: , mcap:, availsupply: to the json item being inserted
+//maybe we update  collection prior to calling the logger.getsummary that holds the responce of api/ticker (ie symbol, index, mcap, supply) which is then cleared once logger is finished to avoid another large db
+function updateData() {
+	logger.getmarketsummaries(function (data, err) {
+		if (err) {
+			throw err;
+		}
+		MongoClient.connect(url, function (error,db) {
+			if (error) {
+				throw error;
+			}
+			data.result.forEach(function (item) {
+				db.collection('markets').insert(item, function (err) {
+					if (err) {
+						throw err;
+					}
+					var id = item._id
+					//should give id of the latest entry inserted to db
+					console.log(id);
+				)};
+			});
+			console.log("Entry Created");
+			db.close();
+		});
+	});
 }
+
+//mongoose can maybe do what I need more effeciently, with schema and models.
 
 setInterval(function () {
     updateData();
-}, 60 * 15 * 1000);
+}, 60 * 0.5 * 1000);
     
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
     res.send('Use /markets');
 
 });
@@ -104,5 +134,5 @@ app.delete('/markets/:_id', function (req, res) {
     });
 });
 
-app.listen(3000);
+app.listen(3000); */
 console.log('Running on port 3000...');
