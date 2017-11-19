d0cc574e92552157693a119940dc5a4648963378
ItsBropez <itsbropez@gmail.com>
2017-11-18 22:30:03 -0700

    a

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ setInterval(function () {
     runner().then(function (resp) {
         console.log(resp);
     });
-}, 60 * 1 * 1000);
+}, 60 * 15 * 1000);
 
 console.log('Running on port 3000...');
 /*
