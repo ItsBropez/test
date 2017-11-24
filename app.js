8db77c0acc48472a17e1ff58f5a6930d371a4612
ItsBropez <itsbropez@gmail.com>
2017-11-24 10:09:39 -0700

    a

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ function parseBittrex(input) {
                 reject(err);
             }
             output.forEach(function (item) {
-                db.collection('data').findOne({"symbol": "'" + item.MarketName + "'"}, function (err, result) {
+                db.collection('data').findOne({"symbol": [item.MarketName]}, function (err, result) {
                     if (err) {
                         console.log(item.MarketName);
                     } else if (result === null) {
