1c688ded21c72c2da23b4338f7a9d9a2cfde810e
ItsBropez <itsbropez@gmail.com>
2017-11-24 10:47:16 -0700

    a

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ function logMarketCap(input) {
                     console.log("i = " + i);
                     }
                 });
-                if (i == input.length - 1) {
+                if (i == (input.length - 1)) {
                     db.close();
                     resolve();
                     console.log("Complete MCAP Log");
